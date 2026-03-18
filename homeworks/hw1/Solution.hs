ed53ca47c52923b1ed05923fc06b4a117dd01dd7
HJaczynski <hubert.jaczynski@gmail.com>
2026-03-18 14:59:51 +0100

    add last 4 parts

--- homeworks/hw1/Solution.hs
+++ homeworks/hw1/Solution.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 --  helper for 1st task
 -- isPrime :: Int -> Bool
 -- isPrime n
@@ -63,16 +64,76 @@ merge (x:xs) (y:ys)
 hamming :: [Integer]
 hamming = 1 : merge (map (*2) hamming) (merge (map (*3) hamming) (map (*5) hamming))
 
+-- Task 7: Integer Power with Bang Patterns 
+power :: Int -> Int -> Int
+power base exponent = go base exponent 1
+  where
+    go _ 0 !acc = acc
+    go b e !acc = go b (e - 1) (acc * b)
+
+-- Task 8, seq version
+listMaxSeq :: [Int] -> Int
+listMaxSeq [] = error "Empty list"
+listMaxSeq (x:xs) = go xs x
+  where
+    go [] acc = acc
+    go (y:ys) acc = let newMax = max y acc 
+                    in newMax `seq` go ys newMax
+
+-- Bang patterns 
+listMaxBang :: [Int] -> Int
+listMaxBang [] = error "Empty list"
+listMaxBang (x:xs) = go xs x
+  where
+    go [] !acc = acc
+    go (y:ys) !acc = go ys (max y acc)
+
+-- Task 9: inf prime stream 
+primes :: [Int]
+primes = sieve [2..]
+
+-- b) part 
+isPrimeInf :: Int -> Bool
+isPrimeInf n
+    | n <= 1    = False
+    | otherwise = n `elem` takeWhile (<= n) primes
+
+-- task 10: Strict accumulations and space leaks 
+-- no strictness annotations
+meanLazy :: [Double] -> Double
+meanLazy xs = let (totalSum, totalLen) = go xs 0 0 in totalSum / totalLen
+  where
+    go [] s l = (s, l)
+    go (y:ys) s l = go ys (s + y) (l + 1)
+
+-- fixed space leak with bang patterns
+meanStrict :: [Double] -> Double
+meanStrict xs = let (totalSum, totalLen) = go xs 0 0 in totalSum / totalLen
+  where
+    go [] !s !l = (s, l)
+    go (y:ys) !s !l = go ys (s + y) (l + 1)
+
+-- Mean and variance in one pass
+meanAndVar :: [Double] -> (Double, Double)
+meanAndVar xs = 
+    let (sumX, sumSq, count) = go xs 0 0 0
+        mu = sumX / count
+        var = (sumSq / count) - (mu * mu)
+    in (mu, var)
+  where
+    go [] !s !sq !n = (s, sq, n)
+    go (y:ys) !s !sq !n = go ys (s + y) (sq + y * y) (n + 1)
+
 main :: IO ()
 main = do
-    -- print (goldbachPairs 4)
-    -- print (goldbachPairs 28)
-    -- print (goldbachPairs 100)
-    -- print (coprimePairs [2, 4, 3, 6, 5])
-    -- print (isPrime 2)
-    -- print (isPrime 13)
-    -- print (isPrime 15)
-    -- print (primesTo 20)
+    print (goldbachPairs 4)
+    print (goldbachPairs 28)
+    print (goldbachPairs 100)
+    print (coprimePairs [2, 4, 3, 6, 5])
+    print (isPrime 2)
+    print (isPrime 13)
+    print (isPrime 15)
+    print (primesTo 20)
     let a = [[1, 2, 3], [4, 5, 6]]
     let b = [[7, 8], [9, 10], [11, 12]]
     print (matMul a b)
@@ -80,3 +141,15 @@ main = do
     print (permutations 3 "abc")
     print (merge [1, 3, 5] [2, 3, 4])
     print (take 20 hamming)
+    print (listMaxSeq [3, 1, 4, 1, 5, 9])
+    print (listMaxBang [3, 12, 4, 1, 5, 9])
+    print (take 20 primes)
+    print (isPrimeInf 29)
+    print (isPrimeInf 30)
+    print (meanLazy [1.0, 2.0, 3.0, 4.0])
+    print (meanStrict [1.0, 2.0, 3.0, 4.0])
+    print (meanAndVar [1.0, 2.0, 3.0, 4.0])
+    print(power 2 10)
+    print(power 3 5)
+
+    
